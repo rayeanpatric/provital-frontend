ddd1ee255ec2a0f70b39d799d3f247f24aeafeb8
Rayean Patric <75995758+rayeanpatric@users.noreply.github.com>
2025-06-11 15:26:29 +0530

    Added blurred circle Effect, with purple shade of opacity 0.25

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Body from "./components/Body/Body";
 import Footer from "./components/Footer/Footer";
+import BackgroundCircles from "./components/BackgroundCircles/BackgroundCircles";
 import "./styles/App.scss";
 
 function App() {
   return (
     <div className="app">
-      <main className="main">
-        <Header />
+      <BackgroundCircles />
+      <Header />
+      <main>
         <Hero />
         <Body />
-        <Footer />
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/BackgroundCircles/BackgroundCircles.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./BackgroundCircles.scss";
+
+const BackgroundCircles = () => {
+  return (
+    <div className="background-circles">
+      <div className="circle circle-1"></div>
+      <div className="circle circle-2"></div>
+      <div className="circle circle-3"></div>
+    </div>
+  );
+};
+
+export default BackgroundCircles;
